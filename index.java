1afc9dc6e3cb59659eb9de420830bef55eab3118
Shael05 <153968970+MAShael07@users.noreply.github.com>
2026-06-07 19:24:19 +0530

    Started Java

---
+++ index.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+class Arraylist{
+    public static void main(String[] args) {
+        ArrayList <Integer> list= new ArrayList <Integer> ();
+        list.add(7);
+        list.add(11);
+        list.add(10);
+        // System.out.println(list);
+        int ele = list.get(0);
+        System.out.println(ele);
+        list.add(1,8);
+        System.out.println(list);
+        list.set(1,5);
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        int size = list.size();
+        System.out.println(size);
+        for(int i =0; i<size; i++){
+            System.out.print(list.get(i));
+        }
+        System.out.println();
+
+        Collections.sort(list);
+        System.out.print(list);
+    }
+
+}
